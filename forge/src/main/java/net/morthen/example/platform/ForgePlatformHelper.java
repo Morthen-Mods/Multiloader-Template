4aae9703ecf2f17ca1b105524714f4302fe4960c
Stein-N <nico-stein-98@sbvl.de>
2026-07-22 17:53:50 +0200

    add forge

---
+++ forge/src/main/java/net/morthen/example/platform/ForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package net.morthen.example.platform;
+
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+import net.morthen.example.platform.services.IPlatformHelper;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "NeoForge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
